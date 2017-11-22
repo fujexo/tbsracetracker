1cb775d88cce73c9842f11731e2573ccabfb8c2a
Philipp Marmet <philipp.marmet@adfinis-sygroup.ch>
2017-11-22 18:26:31 +0100

    Got feedback from actual devs, rebuilt test stuff

--- tbsracetracker/test_tbsracetracker.py
+++ tbsracetracker/test_tbsracetracker.py
@@ -9,45 +9,49 @@ import bluepy
 from tbsracetracker.tbsracetracker import tbstracker
 
 
-class TestTracker():
+@pytest.fixture
+def tracker():
+    return tbstracker('00:00:00:00:00:00')
 
-    @pytest.fixture(autouse=True)
-    def setup_tracker_device(self):
-        self.tracker = tbstracker('00:00:00:00:00:00')
 
-    def test_connect(self, monkeypatch):
-        # Patch the bluepy Peripheral connect function because there is no
-        # actual bluetooth LE device.
-        monkeypatch.setattr(bluepy.btle.Peripheral, 'connect',
-                            MagicMock(bluepy.btle.Peripheral))
+def test_connect(monkeypatch, tracker):
+    # Patch the bluepy Peripheral connect function because there is no
+    # actual bluetooth LE device.
+    monkeypatch.setattr(bluepy.btle.Peripheral, 'connect',
+                        MagicMock(bluepy.btle.Peripheral))
 
-        # Patch the _connected function to return True because we can
-        # not connect to an actual device
-        monkeypatch.setattr(tbstracker, '_connected', MagicMock(tbstracker))
-        rt = tbstracker
-        rt._connected.return_value = True
+    # Patch the _connected function to return True because we can
+    # not connect to an actual device
+    monkeypatch.setattr(tbstracker, '_connected', MagicMock(tbstracker))
+    rt = tbstracker
+    rt._connected.return_value = True
 
-        assert(self.tracker.connect() is True)
+    assert tracker.connect()
 
-    def test_connected(self, monkeypatch):
-        # Patch the bluepy Peripheral status function because there is no
-        # actual bluetooth LE device.
-        monkeypatch.setattr(bluepy.btle.Peripheral, 'status',
-                            MagicMock(bluepy.btle.Peripheral))
-        rt = bluepy.btle.Peripheral
-        rt.status.return_value = {'dst': ['00:00:00:00:00:00'],
-                                  'mtu': [0],
-                                  'rsp': ['stat'],
-                                  'sec': ['low'],
-                                  'state': ['conn']}
 
-        assert(self.tracker._connected() is True)
+def test_connected(monkeypatch, tracker):
+    # Patch the bluepy Peripheral status function because there is no
+    # actual bluetooth LE device.
+    monkeypatch.setattr(bluepy.btle.Peripheral, 'status',
+                        MagicMock(bluepy.btle.Peripheral))
+    rt = bluepy.btle.Peripheral
+    rt.status.return_value = {'dst': ['00:00:00:00:00:00'],
+                              'mtu': [0],
+                              'rsp': ['stat'],
+                              'sec': ['low'],
+                              'state': ['conn']}
 
-    def test_connected_nobluetooth(self, monkeypatch):
-        assert(self.tracker._connected() is False)
+    assert tracker._connected()
 
-    def test_encode_value(self):
-        assert self.tracker._encode_value('t') == b't'
 
-    def test_decode_value(self):
-        assert self.tracker._decode_value(b't') == 't'
+def test_connected_nobluetooth(monkeypatch, tracker):
+    assert not tracker._connected()
+
+
+def test_encode_value(tracker):
+
+    assert tracker._encode_value('t') == b't'
+
+
+def test_decode_value(tracker):
+    assert tracker._decode_value(b't') == 't'
